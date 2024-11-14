f0c17064c9a6ed838ff556a64711223885f2b460
Albirozado <albirosaldoalfredo12@gmail.com>
2024-11-14 22:00:29 +0200

    allow

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ const mongoString = process.env.DATABASE_URL
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 mongoose.connect(mongoString)
 const database = mongoose.connection
 
